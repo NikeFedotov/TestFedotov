3513a0f7d63e423e8c55cd567e06f8df429b8732
Nike Fedotov <fen1kser@yandex.ru>
2024-09-23 21:29:38 +0300

    Initial commit

---
+++ TestFedotov/TestFedotov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestFedotov
+//
+//  Created by Nikita Fedotov on 23.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
